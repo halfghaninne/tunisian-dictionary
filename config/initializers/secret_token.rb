6080656735e7b1fe7172f44f70c877ccb46745ea
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-07-21 11:27:14 -0500

    initial commit. adds file structure for RoR app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TunisianDictionary::Application.config.secret_token = '58ad74ec39e1a3c583551e792f49bc8e45cb0691812d1324a144b2fb2fd2e4ac9109b1b532aaa4b4f456d2f2161d8ccb98497907028b043e88a5c7c8e7cadf20'
